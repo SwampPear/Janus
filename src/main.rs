966f15854517095cf9fd1991f21233eab2cc86b5
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-13 19:48:04 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use dioxus::prelude::*;
-
-
 fn main() {
-    dioxus::desktop::launch(app);
-}
-
-fn app(cx: Scope) -> Element {
-    cx.render(rsx! (
-        div { "Hello, world!" }
-    ))
+  println!("test");
 }
